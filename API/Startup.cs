7e2b7b4ae1d048df5f0953e5c0cedbeeb9649591
I Develop <katlegobenlephadi@gmail.com>
2021-06-24 07:23:45 +0200

    End Of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
             services.AddDbContext<DataContext>(opts=>{
                 opts.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +47,9 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(policy=>{
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
 
             app.UseAuthorization();
 
